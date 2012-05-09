65cf14be2a03a629537faaad9f77656664dbb648
john <mentatdewd@comcast.net>
2012-05-09 11:53:25 -0700

    "Initial install"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JCarrollOnline::Application.config.secret_token = 'a3d2489967b1d8530c3e17b05d6012d695eb27637b58a28a9bc9918c8cc8fadbdb943b874b74ae6c70497f2fad7ce83a23e7b43c272cae15a4c75910400cd681'
