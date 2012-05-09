e02ce15bb95a8fcfea6219ffe182f5d7f86de28c
john <mentatdewd@comcast.net>
2012-05-09 13:46:56 -0700

    Update the entire site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "JCarrollOnline"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+  def safe_image_tag(source, options = {})
+    source ||= "default.jpg"
+    image_tag(source, options)
+  end
 end
