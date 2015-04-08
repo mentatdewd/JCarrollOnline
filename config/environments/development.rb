0e5a6cea6e17867d8fd89402c157a2b4efaf20f3
mentatdewd <mentatdewd@comcast.net>
2015-04-08 12:18:13 -0700

    Updated action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,15 +38,16 @@ JCarrollOnline::Application.configure do
   config.sass.line_comments = false
 
   require 'gmail'
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
-
-  # Include your app's configuration here:
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => "587",
-      :domain => "JCarrollOnline.com", #Example: blog.local
-      :authentication => :plain,
-      :user_name => "jcarrollonline",
-      :password => "floyd1a1a"
-  }
+# Disable delivery errors, bad email addresses will be ignored
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.default_url_options = { :host => "localhost:3000" }
+ActionMailer::Base.smtp_settings = {
+  :address    => "smtp.sendgrid.net",
+  :port       => 25,
+  :user_name  => ENV['SENDGRID_USERNAME'],
+  :password   => ENV['SENDGRID_PASSWORD'],
+  :domain     => ENV['SENDGRID_DOMAIN'],
+  :authentication  => :plain
+}  end
 end
